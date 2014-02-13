9ce662499f63015a75837bfa0b8ae8faaaab0056
Alexander Fosseidbraaten <fosseidb@stud.ntnu.no>
2014-02-13 12:48:44 -0800

    ajax lab 02

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,21 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	var detail = $(this).next();
+
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/"+idNumber, function(result){
+		var projectHTML = '<img src="'+ result['image'] + '" class="detailsImage">' +
+		'<p><small>' + result['date'] + '</small></p>'
+		+'<p>' + result['summary']+'</p>';
+		detail.html(projectHTML);
+	});
+	console.log("/project/"+idNumber);
+}
+
+
+function serverResponse(result, detail) {
+
 }
 
 /*
@@ -35,4 +49,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", function(result) {
+		var colors = result.colors.hex;
+		console.log("derp");
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
